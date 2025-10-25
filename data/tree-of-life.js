57ee0d54d84e64068e48584eb6a10804021ce229
lvsl123 <72800908+lvsl123@users.noreply.github.com>
2025-10-25 14:00:39 +0800

    Tree of life name added
    Yay!! Finally I don't need to bother with names and go looking for Kether, Chokmah, etc

--- data/tree-of-life.js
+++ data/tree-of-life.js
@@ -8,61 +8,61 @@ let treeOfLifeData = {
 	- Destruction through inaction and procrastination <br>
 	- Timelessness, time does not really matter<br>`,
 
-    "001": `Position 1:<br> 
+    "001": `Position 1: Kether/Crown<br> 
 	- Cards: Magician, Lust, Universe<br>
 	- World 1 - the bright light, spiritual and more astral<br>
 	- The light pole that connects directly with number 2<br>
 	- It is also the right pole itself<br>
 	- In the 00x place it represents figures more than actual people<br>`,
 
-    "002": `Position 2:<br>
+    "002": `Position 2: Chokmah/Wisdom<br>
 	- Cards; Priestess, Hanged Man <br>
   - World 1 - the bright light, spiritual and more astral<br>
   - Darkness in the form of the unknown, specifically about creation, therefore also revolves around conflict, unsure, and mental battle<br>
   - The physical stays bright and therefore this is in the right pole<br>
   - Govens more on mental issues than physical issues<br>`,
 
-    "003": `Position 3: <br>
+    "003": `Position 3: Binah/Understanding<br>
   - Cards: Empress, Death<br>
   - World 1- the bright light but more concrete and stable<br>
   - Stability in exchange of unknown conflicts and spirituality<br>
   - The conflict in number 2 becomes more settle, defined and more robust<br>
   - The dark pole is more concrete that comes from concepts that were and are still abstract<br>`,
 
-    "004": `Position 4: <br>
+    "004": `Position 4: Chested/Mercy<br>
   - Cards: Emperor, Art/Temperance<br>
   - World 2 - The more concrete form of world 1. A settled world that is more realistic<br>
   - Opposite polarity of World 1 but the same "position" of position 3<br>
   - Basically the same thing from position 3 but given something that is more mature and lifelier than World 1. <br>
   - Life starts to attempt to go full in bloom in here<br>`,
 
-    "005": `Position 5: <br>
+    "005": `Position 5: Geburah/Strength<br>
   - Cards: Hierophant, Devil<br>
   - World 2 - The more concrete form of world 1. The settled world that goes towards an attempt to become stable<br>
   - We are aiming to finally shed new light to the living world around us.<br>
   - It does not matter how things just come to place. With no rules at all trying to establish them, or to break the rules that became old.<br>`,
 
-    "006": `Position 6:<br>
+    "006": `Position 6: Tipharet/Beauty<br>
   - Cards: Lovers, Tower<br>
   - World 2: In here it can be look as concrete being even more concrete. However the cause and its effect take its full toll at this poisition. Means Devil is directly associate to Tower, and Lovers is directly associated to Hierophant<br>
   - When finally the new Light takes place, we say that this is what is happening to us until we come to the next stage to settle with either the happiness, or the mess that was left behind.<br>`,
 
-    "007": `Position 7: 
+    "007": `Position 7: Netzach/Victory<br> 
   - Cards: Chariot, Star (Date of the sun)<br>
   - World 3: The first 6 are more about our survival instinct, but the 7,8,9 are more about something that we go beyond. We are always searching for things, and we will want to enrich ourselves with other stuff that are equally important as survival, so that we can use this layer to sustain the rest of the branches in return. Otherwise if we stop here, the whole branch will be killed.  <br>
   - It said in here - Move on. Chariot is more personal. But star is more relevant to most people, as it relates to the physical position we are in in the Universe.<br>`,
 
-    "008": `Position 8:
+    "008": `Position 8: Hod/Splendor<br>
   - Cards: Adjustment/Justice, Moon (Date of the Moon, relate to four seasons in Earth better than the western calendar)<br>
   - World 3: Other than survival, there are other challenges when we start to develop. Overall it gives us an advantage over other animals simply because of our own "look empty" attempt that never appeared to be related to survival. But later, such "unrelated" task became our tool to survive in this world.<br>
   - That was why there exist such position because not only we should physically adapt to something that looks completely different from the animal world, but we need to mentally adjust to it. We cannot just hunt for food. There's no place to hunt. Instead we now work in a factory to get money to buy food.<br>`,
 
-    "009": `Position 9: <br>
+    "009": `Position 9: Yesod/Foundation/<br>
   - Cards: Hermit, Sun<br>
   - World 3: Other than survival there are other things we need to fact as a whole world. The One unit that keeps us and our lives enriched, and lifts the human race up to the top of the animal chain. <br>
   - Both cards are basically the same principle but one is individual and other is global.<br>`,
 
-    "010": `Position 10 (refer to 0 for basic stuff): <br>
+    "010": `Position 10 (refer to 0 for basic stuff): Malkuth/Kingdom<br>
   - Cards: Wheel of Fortune, Aeon/Judgement<br>
   - If Fool is "0", then both cards are "1".<br>
   - A contentless space that mysteriously become content rich. Look down as to why we are here to survive, is something at the same time, sweet. Seriously, how did that happen at the first place? We needed none of that if our mind was really only about survival at the first place<br>
